98bd7d468dba8111e2a52ab9dd53902e5ca6f0e4
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-26 20:00:37 +0530

    Remove all Google AdSense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,13 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947456082690864"
-          crossOrigin="anonymous"
-        />
-      </head>
+      <head />
       <body>
         {children}
         <Analytics />
