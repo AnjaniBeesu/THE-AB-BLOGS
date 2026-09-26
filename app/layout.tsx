4340ae852d879e73447199cb31a70b8a5b346a53
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-26 14:54:07 +0530

    Add Google AdSense verification code to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947456082690864"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         {children}
         <Analytics />
