32dba02bbfc7dfe1afc1eb7554774c63ece4cbfc
ANJANI  BEESU <anjanibeesu@gmail.com>
2026-09-26 14:22:18 +0530

    Remove welcome post

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -50,20 +50,6 @@ export const blogs: Blog[] = [
       "And if you are still staring at the list thinking, \"But which one should I pick?\" Pick the one that makes you want to open your editor and build something tonight.",
       "Because the best first programming language is not necessarily the most powerful, the newest or the one with the highest salary. It is the one you will actually use long enough to stop being a beginner."
     ]
-  },
-  {
-    slug: "welcome-to-ab-blogs",
-    title: "Welcome to AB Blogs",
-    excerpt: "A tiny corner of the internet for the things I build, break, learn, and become unnecessarily obsessed with.",
-    date: "2026-09-25",
-    readTime: "2 min read",
-    tags: ["meta", "web", "building"],
-    body: [
-      "I wanted somewhere to write about tech without pretending every experiment needs to become a serious case study.",
-      "So this is AB Blogs: random tech shit by Anjani Beesu. I am a CS sophomore exploring the internet one rabbit hole at a time.",
-      "Some posts will be polished. Some will be chaotic. A few will probably start with me trying one small thing and somehow ending up three hours deep in documentation.",
-      "The goal is simple: build interesting things, understand what I am building, and document the process along the way."
-    ]
   }
 ];
 
